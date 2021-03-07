c4fb7b2d7f8974d7fcbbb1b85cb09fe0954e2225
Aswad Abbas <aswad.imvu@gmail.com>
2021-03-07 16:12:35 +0500

    init

---
+++ src/main/java/com/upstart/ChootayAswad/ChootayAswadApplication.java
@@ -0,0 +1,13 @@
+package com.upstart.ChootayAswad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChootayAswadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChootayAswadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upstart/ChootayAswad/ChootayAswadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upstart.ChootayAswad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChootayAswadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
